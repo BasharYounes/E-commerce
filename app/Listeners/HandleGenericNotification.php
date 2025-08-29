a2d41d2baa2e29d46307cea5944d1620acd56a22
BasharYounes <bywns3777@gmail.com>
2025-08-29 17:28:14 +0300

    Correct problems NotificationService not found

--- app/Listeners/HandleGenericNotification.php
+++ app/Listeners/HandleGenericNotification.php
@@ -5,7 +5,7 @@ namespace App\Listeners;
 use App\Events\GenericNotificationEvent;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
-use NotificationService;
+use App\Services\Notifications\NotificationService;
 
 class HandleGenericNotification
 {
